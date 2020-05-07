6cc703fd1d7755cbc18fe7b0a7019721170fd677
Samuele Milanesi <sam@pop-os.localdomain>
2020-05-07 10:06:02 +0200

    fading-nav js completato

--- public/js/fading-nav.js
+++ public/js/fading-nav.js
@@ -1,53 +1,58 @@
 var fadedIn = false;
-var menuOpened = false; 
+var menuOpened = false;
 var navButton = document.getElementById("nav-button");
 var scroll = 0;
 
 
 $(window).scroll(function (event) {
-  scroll = $(window).scrollTop();
+    scroll = $(window).scrollTop();
+    console.log(scroll);
+    
 });
 
 
-$(window).scroll(function(){
-    if($(this).scrollTop() > 150 && !fadedIn && !menuOpened){
-        fadeinNav();
-    }
-  
-    if($(this).scrollTop() < 20 && (fadedIn || menuOpened)){ 
-        console.log("scopaio");
-        
-        fadeoutNav();
+$(window).scroll(function () {
+    if (!this.menuOpened) {
+        if ($(this).scrollTop() > 150 && !fadedIn) {
+            fadeinNav();
+        }
+
+        if ($(this).scrollTop() < 20 && fadedIn) {
+            console.log("scopaio");
+            fadeoutNav();
+        }
     }
 });
-  
-fadeinNav = function(){
+
+fadeinNav = function () {
     $('nav').removeClass('nav-bg-trasparent');
     $('nav').addClass('nav-bg-green');
     fadedIn = true;
 }
 
-fadeoutNav = function(){
+fadeoutNav = function () {
     $('nav').removeClass('nav-bg-green');
     $('nav').addClass('nav-bg-transparent');
     fadedIn = false;
 }
 
 
-navButton.onclick = function(){
-    if(!menuOpened){
-        if(!fadedIn){
-            fadeinNav();
+navButton.onclick = function () {
+    if (!menuOpened) {
+        if (!fadedIn) {
+            if(!fadedIn){
+                fadeinNav();
+            }
         }
-        else{
+    }
+    else {
+        console.log("sono qui");
+        
+        if (scroll < 150) {
             fadeoutNav();
         }
     }
-    else{
-        if(scroll<150 ){
-            fadeoutNav();
-        }
-    }
+    menuOpened = !menuOpened
 };
 
 
